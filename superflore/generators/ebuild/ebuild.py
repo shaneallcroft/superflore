f00ce6e87a346df70e0018a3605a64f489fb7e15
Shane Byrd Allcroft <shaneallcroft@gmail.com>
2017-11-15 01:01:45 -0500

    Fixed the errors given by travis tests

--- superflore/generators/ebuild/ebuild.py
+++ superflore/generators/ebuild/ebuild.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 from time import gmtime, strftime
 
 from superflore.exceptions import UnresolvedDependency
